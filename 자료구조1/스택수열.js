d48340a4f3557583ca8809a15cef7f1d0abe40e0
juhye <wonjh0730@naver.com>
2021-01-05 01:44:16 +0900

    스택수열 다른 풀이

--- 자료구조1/스택수열.js
+++ 자료구조1/스택수열.js
@@ -37,3 +37,46 @@ if (result === 'NO') {
 } else {
   console.log(result.slice(0, -1));
 }
+
+// 다른 풀이
+// var fs = require('fs');
+// var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// let array = input.map((element) =>
+// Number(element))
+
+// let length = array.shift()
+// let stack = []
+// let flag = []
+
+// function stackSequence(){
+
+//   let i = 0;
+//   while(length >= i){
+
+//     /* stack의 top 확인하기 */
+//     if(stack.length > 0 && array[0] !== undefined
+//     &&stack.slice(-1)[0] === array[0]){
+//       stack.pop()
+//       flag += '-\n'
+//       array.shift()
+//     }else{
+//       i++
+//       stack.push(i)
+//       flag += '+\n'
+//     }
+//   }
+//   stack.pop()
+//   flag = flag.slice(0, -3)
+
+//   if(stack.length > 0){
+//     return 'NO'
+//   }
+
+//   return flag
+// }
+
+// let result = stackSequence()
+// if(result === 'NO'){ console.log(result)}
+// else{
+//     console.log(result)
+// }
